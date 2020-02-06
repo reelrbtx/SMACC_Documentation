6f95e2e79b9678f4871e30885ed92f327f8768a4
travis <travis>
2020-02-06 09:08:42 +0000

    gh-pages travis

--- master/html/search/classes_e.js
+++ master/html/search/classes_e.js
@@ -1,7 +1,5 @@
 var searchData=
 [
   ['unhold',['UNHOLD',['../structsm__packML_1_1StHeld_1_1UNHOLD.html',1,'sm_packML::StHeld']]],
-  ['unhold',['UNHOLD',['../structsm__calendar__week_1_1StHeld_1_1UNHOLD.html',1,'sm_calendar_week::StHeld']]],
-  ['unsuspend',['UNSUSPEND',['../structsm__calendar__week_1_1StSuspended_1_1UNSUSPEND.html',1,'sm_calendar_week::StSuspended']]],
   ['unsuspend',['UNSUSPEND',['../structsm__packML_1_1StSuspended_1_1UNSUSPEND.html',1,'sm_packML::StSuspended']]]
 ];
