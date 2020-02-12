4c26c90a46eff4adffdb5474861beb00550c0c39
travis <travis>
2020-02-12 23:41:08 +0000

    gh-pages travis

--- master/html/search/classes_8.js
+++ master/html/search/classes_8.js
@@ -4,6 +4,7 @@ var searchData=
   ['msdancebotrecoverymode',['MsDanceBotRecoveryMode',['../classsm__dance__bot__strikes__back_1_1MsDanceBotRecoveryMode.html',1,'sm_dance_bot_strikes_back']]],
   ['msdancebotrunmode',['MsDanceBotRunMode',['../classsm__dance__bot__strikes__back_1_1MsDanceBotRunMode.html',1,'sm_dance_bot_strikes_back']]],
   ['msdancebotrunmode',['MsDanceBotRunMode',['../classsm__dance__bot_1_1MsDanceBotRunMode.html',1,'sm_dance_bot']]],
+  ['msrecharge',['MsRecharge',['../classsm__pr2__plugs_1_1MsRecharge.html',1,'sm_pr2_plugs']]],
   ['msrecover',['MsRecover',['../classsm__three__some_1_1MsRecover.html',1,'sm_three_some']]],
   ['msrecoverymode',['MsRecoveryMode',['../structsm__viewer__sim_1_1MsRecoveryMode.html',1,'sm_viewer_sim']]],
   ['msrun',['MsRun',['../classsm__three__some_1_1MsRun.html',1,'sm_three_some']]],
@@ -13,6 +14,5 @@ var searchData=
   ['msweekend',['MsWeekend',['../classsm__pr2__plugs_1_1MsWeekend.html',1,'sm_pr2_plugs']]],
   ['msweekend',['MsWeekend',['../classsm__calendar__week_1_1MsWeekend.html',1,'sm_calendar_week']]],
   ['msworkweek',['MsWorkweek',['../classsm__calendar__week_1_1MsWorkweek.html',1,'sm_calendar_week']]],
-  ['msworkweek',['MsWorkweek',['../classsm__pr2__plugs_1_1MsWorkweek.html',1,'sm_pr2_plugs']]],
   ['my_5fcontext',['my_context',['../structsmacc_1_1SmaccState_1_1my__context.html',1,'smacc::SmaccState']]]
 ];
--- master/html/search/classes_9.js
+++ master/html/search/classes_9.js
@@ -1,8 +1,8 @@
 var searchData=
 [
-  ['next',['NEXT',['../structsm__three__some_1_1ss1__states_1_1StiState3_1_1NEXT.html',1,'sm_three_some::ss1_states::StiState3']]],
-  ['next',['NEXT',['../structsm__three__some_1_1ss1__states_1_1StiState2_1_1NEXT.html',1,'sm_three_some::ss1_states::StiState2']]],
-  ['next',['NEXT',['../structsm__three__some_1_1StState2_1_1NEXT.html',1,'sm_three_some::StState2']]],
+  ['next',['NEXT',['../structsm__three__some_1_1StState1_1_1NEXT.html',1,'sm_three_some::StState1']]],
   ['next',['NEXT',['../structsm__three__some_1_1StState3_1_1NEXT.html',1,'sm_three_some::StState3']]],
-  ['next',['NEXT',['../structsm__three__some_1_1StState1_1_1NEXT.html',1,'sm_three_some::StState1']]]
+  ['next',['NEXT',['../structsm__three__some_1_1StState2_1_1NEXT.html',1,'sm_three_some::StState2']]],
+  ['next',['NEXT',['../structsm__three__some_1_1ss1__states_1_1StiState3_1_1NEXT.html',1,'sm_three_some::ss1_states::StiState3']]],
+  ['next',['NEXT',['../structsm__three__some_1_1ss1__states_1_1StiState2_1_1NEXT.html',1,'sm_three_some::ss1_states::StiState2']]]
 ];
