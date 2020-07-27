cd69b52a8844d372c171fcad537baadf23ac894f
travis <travis>
2020-07-27 01:06:58 +0000

    gh-pages travis

--- master/html/search/files_1.js
+++ master/html/search/files_1.js
@@ -1,4 +1,4 @@
 var searchData=
 [
-  ['action_5fserver_5fnode_5f3_2ecpp',['action_server_node_3.cpp',['../servers_2action__server__node__3_2src_2action__server__node__3_8cpp.html',1,'(Global Namespace)'],['../2_2servers_2action__server__node__3_2src_2action__server__node__3_8cpp.html',1,'(Global Namespace)'],['../strikes__back_2servers_2action__server__node__3_2src_2action__server__node__3_8cpp.html',1,'(Global Namespace)']]]
+  ['action_5fserver_5fnode_5f3_2ecpp',['action_server_node_3.cpp',['../sm__dance__bot_2servers_2action__server__node__3_2src_2action__server__node__3_8cpp.html',1,'(Global Namespace)'],['../sm__dance__bot__strikes__back_2servers_2action__server__node__3_2src_2action__server__node__3_8cpp.html',1,'(Global Namespace)'],['../sm__ridgeback__floor__coverage__dynamic__1_2servers_2action__server__node__3_2src_2action__server__node__3_8cpp.html',1,'(Global Namespace)'],['../sm__ridgeback__floor__coverage__static__1_2servers_2action__server__node__3_2src_2action__server__node__3_8cpp.html',1,'(Global Namespace)']]]
 ];
--- master/html/search/files_e.js
+++ master/html/search/files_e.js
@@ -1,5 +1,5 @@
 var searchData=
 [
-  ['temperature_5fsensor_5fnode_2ecpp',['temperature_sensor_node.cpp',['../servers_2temperature__sensor__node_2src_2temperature__sensor__node_8cpp.html',1,'(Global Namespace)'],['../2_2servers_2temperature__sensor__node_2src_2temperature__sensor__node_8cpp.html',1,'(Global Namespace)'],['../strikes__back_2servers_2temperature__sensor__node_2src_2temperature__sensor__node_8cpp.html',1,'(Global Namespace)']]],
+  ['temperature_5fsensor_5fnode_2ecpp',['temperature_sensor_node.cpp',['../sm__dance__bot_2servers_2temperature__sensor__node_2src_2temperature__sensor__node_8cpp.html',1,'(Global Namespace)'],['../sm__dance__bot__strikes__back_2servers_2temperature__sensor__node_2src_2temperature__sensor__node_8cpp.html',1,'(Global Namespace)'],['../sm__ridgeback__floor__coverage__dynamic__1_2servers_2temperature__sensor__node_2src_2temperature__sensor__node_8cpp.html',1,'(Global Namespace)'],['../sm__ridgeback__floor__coverage__static__1_2servers_2temperature__sensor__node_2src_2temperature__sensor__node_8cpp.html',1,'(Global Namespace)']]],
   ['timer_5fclient_2ecpp',['timer_client.cpp',['../timer__client_8cpp.html',1,'']]]
 ];
