4bbcaa29359308e454ae76f57dab8018b5ebadc3
travis <travis>
2020-08-23 09:21:08 +0000

    gh-pages travis

--- master/html/search/enums_0.js
+++ master/html/search/enums_0.js
@@ -1,5 +1,5 @@
 var searchData=
 [
   ['computejointtrajectoryerrorcode',['ComputeJointTrajectoryErrorCode',['../namespacecl__move__group__interface.html#ae5fc1caf9a16ae5ad1c97c2e137a7017',1,'cl_move_group_interface']]],
-  ['cubelocation',['CubeLocation',['../namespacesm__fetch__six__table__pick__n__sort__1_1_1cl__perception__system.html#a371363065945abbb60dad8b56601afa0',1,'sm_fetch_six_table_pick_n_sort_1::cl_perception_system::CubeLocation()'],['../namespacesm__moveit__screw__loop_1_1cl__perception__system.html#a3685ee11048648ccb59214d2341fb8ca',1,'sm_moveit_screw_loop::cl_perception_system::CubeLocation()'],['../namespacesm__moveit__wine__serve_1_1cl__perception__system.html#abb5953d380907809caaf4c1a83eec32c',1,'sm_moveit_wine_serve::cl_perception_system::CubeLocation()']]]
+  ['cubelocation',['CubeLocation',['../namespacesm__fetch__six__table__pick__n__sort__1_1_1cl__perception__system.html#a371363065945abbb60dad8b56601afa0',1,'sm_fetch_six_table_pick_n_sort_1::cl_perception_system::CubeLocation()'],['../namespacesm__fetch__two__table__whiskey__pour_1_1cl__perception__system.html#a7523acd4abcc84c78d6a15965f9c25b9',1,'sm_fetch_two_table_whiskey_pour::cl_perception_system::CubeLocation()'],['../namespacesm__moveit__screw__loop_1_1cl__perception__system.html#a3685ee11048648ccb59214d2341fb8ca',1,'sm_moveit_screw_loop::cl_perception_system::CubeLocation()']]]
 ];
